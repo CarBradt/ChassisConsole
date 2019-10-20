ec32a66b215c120bce99b1e57f61c47d5a3d7d00
Bradtus <bradttom@gmail.com>
2019-10-20 18:12:13 +0200

    just some changes

--- ChassisConsole/Program.cs
+++ ChassisConsole/Program.cs
@@ -2,20 +2,28 @@
 using CefSharp.OffScreen;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Text;
 
 namespace ChassisConsole
 {
     class Program
     {
         private static ChromiumWebBrowser browser;
-        private const string testUrl = "http://www.mobilit.fgov.be/WebdivPub/wmvpstv1.jsp";
         private const string chassisNr = "W0L0XCF6846053363";
+        private const string testUrl = "http://www.mobilit.fgov.be/WebdivPub/wmvpstv1.jsp";
+        private const string targetDocument = @"document.querySelector('#CMV_WD_PBL01_CONSULT_STATUS_FRAME').firstChild.contentWindow.document";
+        private const string inputElementId = "#Writable3";
+        private const string requestButtonId = "#btnOphalen";
         private static bool requested = false;
+        private static Stopwatch timer;
 
 
         static void Main(string[] args)
         {
+            timer = new Stopwatch();
+            timer.Start();
             CefSharpSettings.SubprocessExitIfParentProcessClosed = true;
 
             var settings = new CefSettings()
@@ -28,7 +36,6 @@ namespace ChassisConsole
 
             Console.ReadKey();
             Cef.Shutdown();
-
         }
 
         private static void BrowserLoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
@@ -38,9 +45,8 @@ namespace ChassisConsole
                 if (requested == false)
                 {
                     requested = true;
-                    // fill the username and password fields with their respective values, then click the submit button
-                    var script = $@"document.querySelector('#CMV_WD_PBL01_CONSULT_STATUS_FRAME').firstChild.contentWindow.document.querySelector('#Writable3').value = '{chassisNr}';
-                                document.querySelector('#CMV_WD_PBL01_CONSULT_STATUS_FRAME').firstChild.contentWindow.document.querySelector('#btnOphalen').click();";
+                    var script = $@"{targetDocument}.querySelector('{inputElementId}').value = '{chassisNr}';
+                                    {targetDocument}.querySelector('{requestButtonId}').click();";
 
                     browser.EvaluateScriptAsync(script).ContinueWith(u =>
                     {
@@ -48,26 +54,33 @@ namespace ChassisConsole
                     });
                 }
 
-                var pageQueryScript =
-                @"(function(){
-                    var lis = document.querySelector('#CMV_WD_PBL01_CONSULT_STATUS_FRAME').firstChild.contentWindow.document.querySelectorAll('input');
-                    var result = [];
-                    for(var i=0; i < lis.length; i++) { result.push(lis[i].value) } 
-                    return result; 
-                })()";
-                var scriptTask = browser.EvaluateScriptAsync(pageQueryScript);
-                scriptTask.ContinueWith(u =>
+                else
                 {
-                    if (u.Result.Success && u.Result.Result != null)
+                    var pageQuery = new StringBuilder();
+                    pageQuery.Append(@"(function(){");
+                    pageQuery.Append($@"var lis = {targetDocument}.querySelectorAll('input');");
+                    pageQuery.Append(@"var result = [];");
+                    pageQuery.Append(@"for(var i=0; i < lis.length; i++) { result.push(lis[i].value) } ");
+                    pageQuery.Append(@"return result;");
+                    pageQuery.Append(@"})()");
+                    var scriptTask = browser.EvaluateScriptAsync(pageQuery.ToString());
+                    pageQuery = null;
+
+                    scriptTask.ContinueWith(u =>
                     {
-                        var response = (List<dynamic>)u.Result.Result;
-                        foreach (string v in response)
+                        if (u.Result.Success && u.Result.Result != null)
                         {
-                            if (!string.IsNullOrWhiteSpace(v))
-                                Console.WriteLine(v);
+                            var response = (List<dynamic>)u.Result.Result;
+                            foreach (string v in response)
+                            {
+                                if (!string.IsNullOrWhiteSpace(v))
+                                    Console.WriteLine(v);
+                            }
+                            timer.Stop();
+                            Console.WriteLine($"Elapsed time: {timer.Elapsed.TotalSeconds} seconds");
                         }
-                    }
-                });
+                    });
+                }
             }
         }
     }
